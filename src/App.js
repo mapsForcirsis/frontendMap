0d6c67ca8039c32c0d41087d12833a22951c75c0
Hansson Patrik <pathan@lantm.intra.local>
2020-04-05 13:06:33 +0200

    ready for demo

--- src/App.js
+++ src/App.js
@@ -4,27 +4,39 @@ import MyMap from './Map';
 import Grid from '@material-ui/core/Grid';
 
 class App extends Component {
-  
+
   constructor(props) {
     super(props)
     this.firsttimeitloadsthemap = true
     this.state = {
-      lanName:''
+      lanName: ''
     }
   }
 
-  HandleLanName=(lanName)=>{
-    this.setState({lanName})
+  HandleLanName = (lanName) => {
+    this.setState(oldstate =>{
+      return {
+        ...oldstate, kommunname: "",lanName:lanName
+      }
+    })
+  }
+  HandleKommunName = (kommun) => {
+    this.setState(oldstate => {
+      return {
+        ...oldstate, kommunname: kommun.properties.kommunnamn
+      }
+    })
   }
   render() {
-    let lanName=this.state.lanName
+    let lanName = this.state.lanName
     return (
       <Grid container className="App">
         <Grid item sm={6}> <h1>{lanName}</h1>
-</Grid>
-        <Grid item sm={6}><MyMap LanName={(e)=>this.HandleLanName(e)}/></Grid>
-        
-      </Grid>
+          <Grid item sm={6}> <h2>{this.state.kommunname}</h2></Grid>
+          </Grid>
+          <Grid item sm={6}><MyMap LanName={(e) => this.HandleLanName(e)}  changeKommunName={(e) => this.HandleKommunName(e) }/></Grid>
+
+        </Grid>
     );
   }
 }
--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Map as LeafletMap, TileLayer, Polygon } from 'react-leaflet';
+import { Map as LeafletMap, TileLayer, Polygon, CircleMarker, Tooltip } from 'react-leaflet';
 import L from 'leaflet'
 import { CRS } from 'proj4leaflet';
 let lan = require('./län.json');
-
+let kommun = require('./kommun.json');
+let vardgivareGBG = require('./vardgivare_GBG.json')
 const crs = new L.Proj.CRS('EPSG:3006',
   '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
   {
@@ -18,12 +19,14 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.firsttimeitloadsthemap = true
+    this.vardCords = []
     this.state = {
       lat: 57,
       lng: 18,
       zoom: 5,
       bluemarble: false,
-      clickedLanskod: 0
+      clickedLanskod: 0,
+      clickedKommunkod: 0
     }
   }
   handleMoveend = (e) => {
@@ -31,50 +34,111 @@ class App extends React.Component {
     console.log(e.target.options.center);
     this.firsttimeitloadsthemap = false
   }
-  lanClick=(e)=>{
-
-    this.setState(old =>{
-      console.log(e.properties.lanskod)
-      return {...old, clickedLanskod: e.properties.lanskod}
+  lanClick = (e, event) => {
+    // this.map.props.zoom=3
+    this.map.leafletElement.setView([event.latlng.lat, event.latlng.lng], 2)
+    // this.map.setView([event.latlng.lat, event.latlng.lng]);
+    this.setState(old => {
+      return { ...old, clickedLanskod: e.properties.lanskod }
     })
-      let lanName= e.properties.lansnamn
-      this.props.LanName(lanName)
+    let lanName = e.properties.lansnamn
+    this.props.LanName(lanName)
+  }
+  kommunClick = (kommun, event) => {
+    this.map.leafletElement.setView([event.latlng.lat, event.latlng.lng], 6)
+
+    //  let vardCords= this.vardCords;
+    // console.log(vardCords)
+    // this.map.leafletElement.fitBounds(vardCords).pad(0.1)
+
+    this.props.changeKommunName(kommun)
+    this.setState(oldstate => {
+      return {
+        ...oldstate, clickedKommunkod: parseInt(kommun.properties.kommunkod, 10)
+      }
+    })
+
   }
 
   render() {
+    return (
 
-return (
+      <LeafletMap center={[62, 18]} zoom={1} crs={crs} ref={(ref) => { this.map = ref; }} >
 
-  <LeafletMap center={[62, 18]} zoom={1} crs={crs}>
+        <TileLayer
 
-    <TileLayer
-      maxZoom={9}
-      minZoom={0}
-      attribution="&amp;copy <a href=&quot;https://www.lantmateriet.se/en/&quot;>Lantmäteriet</a> Topografisk Webbkarta Visning, CCB"
-      url={`https://api.lantmateriet.se/open/topowebb-ccby/v1/wmts/token/474aa0a7-917a-3a55-88fc-72ecb5220ceb/1.0.0/topowebb/default/3006/{z}/{y}/{x}.png`}
-    />
-    {lan.features.map((lanne,i) => {
-let superCord=[]
-    lanne.properties.lansyta.coordinates.forEach(coords => {
-        let ArrCords=[]
-       coords.forEach(arr =>{
-           let ArrOfCord=[]
-           arr.forEach(e=>{
-            ArrOfCord.push(crs.unproject(L.point(e)))
-           })
-           ArrCords.push(ArrOfCord)
-        })
-        superCord.push(ArrCords)        
-      })
-      console.log(lanne.properties.lanskod)
-      console.log(this.state.clickedLanskod)
-     return <Polygon onClick={(e)=>this.lanClick(lanne)} key={i} positions={superCord} color={this.state.clickedLanskod == lanne.properties.lanskod ? "#B8860B" : "green"} />
-    })}
-  </LeafletMap>
+          maxZoom={9}
+          minZoom={0}
+          attribution="&amp;copy <a href=&quot;https://www.lantmateriet.se/en/&quot;>Lantmäteriet</a> Topografisk Webbkarta Visning, CCB"
+          url={`https://api.lantmateriet.se/open/topowebb-ccby/v1/wmts/token/474aa0a7-917a-3a55-88fc-72ecb5220ceb/1.0.0/topowebb/default/3006/{z}/{y}/{x}.png`}
+        />
+        {lan.features.map((lanne, i) => {
+          let superCord = []
+          lanne.properties.lansyta.coordinates.forEach(coords => {
+            let ArrCords = []
+            coords.forEach(arr => {
+              let ArrOfCord = []
+              arr.forEach(e => {
+                ArrOfCord.push(crs.unproject(L.point(e)))
+              })
+              ArrCords.push(ArrOfCord)
+            })
+            superCord.push(ArrCords)
+          })
+          let number = Math.random();
+
+          return <Polygon onClick={(e) => this.lanClick(lanne, e)} key={i} positions={superCord} color={number < 0.5 ? "green" : "red"} />
+        })}
+
+        {
+          kommun.features.map((kommun, i) => {
+            let kommunCord = []
+            kommun.properties.kommunyta.coordinates.forEach(coords => {
+              let ArrCords = []
+              coords.forEach(arr => {
+                let ArrOfCord = []
+                arr.forEach(e => {
+                  ArrOfCord.push(crs.unproject(L.point(e)))
+                })
+                ArrCords.push(ArrOfCord)
+              })
+              kommunCord.push(ArrCords)
+            })
+            let tempString = kommun.properties.kommunkod.slice(0, 2);
+            if (tempString.length == 1) {
+              tempString = "0" + tempString
+            }
+            let kommunkodIntValue = parseInt(tempString, 10);
+            if (kommunkodIntValue == this.state.clickedLanskod) {
+              let number = Math.random();
+              return <Polygon onClick={(e) => this.kommunClick(kommun, e)} key={i} positions={kommunCord} color={ number < 0.5 ? "white" : "blue"} />
+            }
+
+
+          })
+
+        }
+
+        {
+          vardgivareGBG.map((vard, key) => {
+
+            if (this.state.clickedKommunkod == 1480) {
+              let vardCords = this.vardCords;
+              vardCords.push(vard.coords);
+              let number = Math.random();
+              return <CircleMarker key={key} center={vard.coords} color={ number < 0.5 ? "brown" : "gold"} radius={6}>
+                <Tooltip>
+                  {vard.namn}
+                </Tooltip>
+              </CircleMarker>
+            }
+          })
+        }
+      </LeafletMap>
 
 
 
-);
+    );
   }
 }
 
